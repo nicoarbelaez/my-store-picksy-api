ef5275ef4c20c675c1250a2461483e3564f7dec0
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-03-20 16:44:55 -0500

    Agregar manejo de JSON y actualizar rutas en el servidor

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import routerApi from './routes/index.router.js';
 const app = express();
 const PORT = 3000;
 
-app.get('/about', (req, res) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
   res.send('<h1>My Store</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1>');
 });
 
 routerApi(app);
