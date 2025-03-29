82a00d5eaf1606dc71bd8dc1f16f9eb09e304593
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-03-29 17:03:19 -0500

    Actualizar la conexión de Sequelize para usar la URL de la base de datos desde las variables de entorno

--- config/config.js
+++ config/config.js
@@ -4,10 +4,11 @@ export const config = {
   host: process.env.HOST || 'localhost',
   db: {
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5431,
+    port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || 'my_store',
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
+    productionDbUrl: process.env.DB_POSTGRES_URL || null,
   },
   corsWhitelist: process.env.CORS_WHITELIST || '*',
 };
--- db/config.js
+++ db/config.js
@@ -1,10 +1,28 @@
 import { config } from '../config/config.js';
 
-const USER = encodeURIComponent(config.db.user);
-const PASSWORD = encodeURIComponent(config.db.password);
-const URI = `postgres://${USER}:${PASSWORD}@${config.db.host}:${config.db.port}/${config.db.database}`;
+const devUSER = encodeURIComponent(config.db.user);
+const devPASSWORD = encodeURIComponent(config.db.password);
+const devURI = `postgres://${devUSER}:${devPASSWORD}@${config.db.host}:${config.db.port}/${config.db.database}`;
 
 export default {
-  development: { url: URI, dialect: 'postgres' },
-  production: { url: URI, dialect: 'postgres' },
+  development: {
+    url: devURI,
+    dialect: 'postgres',
+  },
+  production: config.env === 'production' && config.db.productionDbUrl
+    ? {
+      url: config.db.productionDbUrl,
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+    : {
+      url: devURI,
+      dialect: 'postgres',
+    },
 };
