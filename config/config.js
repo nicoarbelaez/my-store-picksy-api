ac691575a2f2bc252dad4ce55a354c57d1c0d948
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-04-12 23:44:03 -0500

    Agregar nueva configuración para las variables de entorno

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ export const config = {
   jwtSecret: process.env.JWT_SECRET,
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST || 'http://localhost',
   db: {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5431,
@@ -18,5 +18,15 @@ export const config = {
   imgur: {
     clientId: process.env.IMGUR_CLIENT_ID,
     clientSecret: process.env.IMGUR_CLIENT_SECRET,
+  },
+  mail: {
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT) || 587,
+    secure: process.env.SMTP_SECURE === 'true',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+    from: process.env.SMTP_FROM_EMAIL,
   },
 };
