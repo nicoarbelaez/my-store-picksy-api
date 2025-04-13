bec648689909f199900743635cf2f9e54d130ab5
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-04-12 20:53:26 -0500

    Eliminar lineas no usadas.

--- utils/auth/strategies/jwt.strategy.js
+++ utils/auth/strategies/jwt.strategy.js
@@ -1,9 +1,6 @@
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import UserService from '../../../services/user.service.js';
-import boom from '@hapi/boom';
 import { config } from '../../../config/config.js';
 
-const service = new UserService();
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.jwtSecret,
